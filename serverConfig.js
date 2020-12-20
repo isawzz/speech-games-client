95d952c705bb7944152120eece122e116b5430ef
isawzz <iwgajam@gmail.com>
2020-12-20 05:08:06 -0800

    ok

--- serverConfig.js
+++ serverConfig.js
@@ -6,7 +6,7 @@ const EXPERIMENTAL = false;
 const DEV_MODE = false; //OFFLINE is true auf ASUS ROG soll aber false sein auf github!
 const IS_TESTING = false;
 
-const BROADCAST_SETTINGS = true; // ACHTUNG!!!!!!!!!!!!! true;
+const BROADCAST_SETTINGS = false; // ACHTUNG!!!!!!!!!!!!! true;
 var PROD_START = !OFFLINE;
 
 
