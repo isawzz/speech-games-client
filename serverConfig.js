cdca4b598c563173d0ef872c3b98d66f83aff90a
isawzz <iwgajam@gmail.com>
2021-01-14 18:41:22 -0800

    das

--- serverConfig.js
+++ serverConfig.js
@@ -2,11 +2,11 @@ const OFFLINE = false;
 DEFAULTUSERNAME = 'zoe';
 // const SERVERURL =  OFFLINE ? 'http://localhost:3000/users/' : 'https://speech-games.herokuapp.com/users/';
 const SERVERURL =  OFFLINE ? 'http://localhost:3000/app/' : 'https://speech-games.herokuapp.com/app/';
-const EXPERIMENTAL = false;
+const EXPERIMENTAL = true;
 const DEV_MODE = false; //OFFLINE is true auf ASUS ROG soll aber false sein auf github!
 const IS_TESTING = false;
 
-const BROADCAST_SETTINGS = false; // ACHTUNG!!!!!!!!!!!!! true;
+const BROADCAST_SETTINGS = true; // ACHTUNG!!!!!!!!!!!!! true;
 var PROD_START = !OFFLINE;
 
 
