6ef8a9caef152f5b8fa96723bfc6a6458a15a095
isawzz <iwgajam@gmail.com>
2021-04-12 10:35:37 -0700

    das

--- version0/serverConfig.js
+++ version0/serverConfig.js
@@ -4,7 +4,7 @@ const SERVERURL = OFFLINE ? 'http://localhost:3000/app/' : 'https://speech-games
 const IS_TESTING = false; // *** only set this one! ***
 
 DEFAULTUSERNAME = 'zoe';// IS_TESTING ? 'nil' : 'gul';
-const BROADCAST_SETTINGS = true; //IS_TESTING; // ACHTUNG!!!!!!!!!!!!! true;
+const BROADCAST_SETTINGS = false; //IS_TESTING; // ACHTUNG!!!!!!!!!!!!! true;
 var USE_LOCAL_STORAGE = !BROADCAST_SETTINGS; // true | false //localStorage is cleared when false!!!!!
 const CLEAR_LOCAL_STORAGE = BROADCAST_SETTINGS;
 var PROD_START = !IS_TESTING;
